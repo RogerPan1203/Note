5f57ee82bee8b0b0e201ebe2fc6e135dace469a2
RogerPan1203 <panyf200713@outlook.com>
2024-10-22 23:46:49 -0700

    Initial Commit

---
+++ Note/NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteApp.swift
+//  Note
+//
+//  Created by Roger on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NoteTests/NoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTests.swift
+//  NoteTests
+//
+//  Created by Roger on 10/22/24.
+//
+
+import Testing
+@testable import Note
+
+struct NoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
